7547adfcb724fd7a8ebc02c697eb87b7ce135748
ches <1688276+vorobeyme@users.noreply.github.com>
2024-03-15 09:46:44 +0200

    feat: add languages API functionality (#3)

---
+++ crowdin/languages.go
@@ -0,0 +1,84 @@
+package crowdin
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/crowdin/crowdin-api-client-go/crowdin/model"
+)
+
+// Crowdin supports more than 300 world languages and custom languages created in the system.
+// Use API to get the list of all supported languages and retrieve additional details
+// (e.g. text direction, internal code) on specific language.
+//
+// https://developer.crowdin.com/api/v2/#tag/Languages
+type LanguagesService struct {
+	client *Client
+}
+
+// List returns a list of all supported languages.
+//
+// Query parameters:
+//
+//	limit: A maximum number of items to retrieve (default 25, max 500).
+//	offset: A starting offset in the collection of items (default 0).
+//
+// https://developer.crowdin.com/api/v2/#operation/api.languages.getMany
+func (s *LanguagesService) List(ctx context.Context, opts *model.ListOptions) ([]*model.Language, *Response, error) {
+	res := new(model.LanguagesListResponse)
+	resp, err := s.client.Get(ctx, "/api/v2/languages", opts, res)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	langs := make([]*model.Language, 0, len(res.Data))
+	for _, lang := range res.Data {
+		langs = append(langs, lang.Data)
+	}
+
+	return langs, resp, nil
+}
+
+// Get returns a language by its identifier.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.languages.get
+func (s *LanguagesService) Get(ctx context.Context, id string) (*model.Language, *Response, error) {
+	res := new(model.LanguagesGetResponse)
+	resp, err := s.client.Get(ctx, fmt.Sprintf("/api/v2/languages/%s", id), nil, res)
+
+	return res.Data, resp, err
+}
+
+// Add adds a new custom language.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.languages.post
+func (s *LanguagesService) Add(ctx context.Context, req *model.AddLanguageRequest) (*model.Language, *Response, error) {
+	res := new(model.LanguagesGetResponse)
+	resp, err := s.client.Post(ctx, "/api/v2/languages", req, res)
+
+	return res.Data, resp, err
+}
+
+// Edit updates a custom language by its identifier.
+//
+// Request body:
+//
+//		op: The operation to perform. Enum: replace, test
+//		path: A JSON Pointer as defined in RFC 6901.
+//	          Enum: "/name" "/textDirection" "/pluralCategoryNames" "/threeLettersCode" "/localeCode" "/dialectOf"
+//		value: The value to be used within the operations. The value must be one of string or array of strings.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.languages.patch
+func (s *LanguagesService) Edit(ctx context.Context, id string, req []*model.UpdateRequest) (*model.Language, *Response, error) {
+	res := new(model.LanguagesGetResponse)
+	resp, err := s.client.Patch(ctx, fmt.Sprintf("/api/v2/languages/%s", id), req, res)
+
+	return res.Data, resp, err
+}
+
+// Delete deletes a custom language by its identifier.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.languages.delete
+func (s *LanguagesService) Delete(ctx context.Context, id string) (*Response, error) {
+	return s.client.Delete(ctx, fmt.Sprintf("/api/v2/languages/%s", id))
+}
---
+++ crowdin/model/languages.go
@@ -0,0 +1,93 @@
+package model
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Language defines the structure of a language.
+type Language struct {
+	ID                  string   `json:"id"`
+	Name                string   `json:"name"`
+	EditorCode          string   `json:"editorCode"`
+	TwoLettersCode      string   `json:"twoLettersCode"`
+	ThreeLettersCode    string   `json:"threeLettersCode"`
+	Locale              string   `json:"locale"`
+	AndroidCode         string   `json:"androidCode"`
+	OsxCode             string   `json:"osxCode"`
+	OsxLocale           string   `json:"osxLocale"`
+	PluralCategoryNames []string `json:"pluralCategoryNames"`
+	PluralRules         string   `json:"pluralRules"`
+	PluralExamples      []string `json:"pluralExamples"`
+	TextDirection       string   `json:"textDirection"`
+	DialectOf           string   `json:"dialectOf"`
+}
+
+// LanguagesListResponse defines the structure of a response
+// when getting a list of languages.
+type LanguagesListResponse struct {
+	Data       []*LanguagesGetResponse `json:"data"`
+	Pagination *Pagination             `json:"pagination"`
+}
+
+// LanguagesGetResponse defines the structure of a response
+// when getting a single language.
+type LanguagesGetResponse struct {
+	Data *Language `json:"data"`
+}
+
+// AddLanguageRequest defines the structure of a request
+// to add a new custom language.
+type AddLanguageRequest struct {
+	// Language name.
+	Name string `json:"name"`
+	// Custom language code.
+	Code string `json:"code"`
+	// Custom language locale code.
+	LocaleCode string `json:"localeCode"`
+	// Text direction in custom language. Enum: "ltr" "rtl".
+	//  "ltr" - left-to-right
+	//  "rtl" - right-to-left
+	TextDirection string `json:"textDirection"`
+	// Array with category names.
+	PluralCategoryNames []string `json:"pluralCategoryNames"`
+	// Custom language 3 letters code. Format: ISO 6393 code.
+	ThreeLettersCode string `json:"threeLettersCode"`
+	// Custom language 2 letters code. Format: ISO 6391 code.
+	TwoLettersCode string `json:"twoLettersCode"`
+	// Use if custom language is a dialect.
+	DialectOf string `json:"dialectOf"`
+}
+
+// Validate checks if the add request is valid.
+// It implements the RequestValidator interface.
+func (r *AddLanguageRequest) Validate() error {
+	if r.Name == "" {
+		return errors.New("name is required")
+	}
+	if r.Code == "" {
+		return errors.New("code is required")
+	}
+	if r.LocaleCode == "" {
+		return errors.New("localeCode is required")
+	}
+	if r.ThreeLettersCode == "" {
+		return errors.New("threeLettersCode is required")
+	}
+	if len(r.PluralCategoryNames) == 0 {
+		return errors.New("pluralCategoryNames is required")
+	}
+
+	const (
+		ltr = "ltr"
+		rtl = "rtl"
+	)
+	if r.TextDirection == "" {
+		return errors.New("textDirection is required")
+	}
+	if r.TextDirection != ltr && r.TextDirection != rtl {
+		return fmt.Errorf("textDirection must be %q or %q", ltr, rtl)
+	}
+
+	return nil
+}
