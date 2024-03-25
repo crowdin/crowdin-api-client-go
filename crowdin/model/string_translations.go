f4e75290dbb486fade3810eb1e2c1b6e05751639
ches <1688276+vorobeyme@users.noreply.github.com>
2024-03-26 01:26:28 +0200

    feat: add string translations API functionality

---
+++ crowdin/model/string_translations.go
@@ -0,0 +1,457 @@
+package model
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
+
+// Approval represents a Crowdin translation approval.
+type Approval struct {
+	ID            int    `json:"id"`
+	User          User   `json:"user"`
+	TranslationID int    `json:"translationId"`
+	StringID      int    `json:"stringId"`
+	LanguageID    string `json:"languageId"`
+	CreatedAt     string `json:"createdAt"`
+}
+
+type User struct {
+	ID        int    `json:"id"`
+	Username  string `json:"username"`
+	FullName  string `json:"fullName"`
+	AvatarURL string `json:"avatarUrl"`
+}
+
+// ApprovalsGetResponse defines the structure of the response when
+// getting a single translation approval.
+type ApprovalsGetResponse struct {
+	Data *Approval `json:"data"`
+}
+
+// ApprovalsListResponse defines the structure of the response when
+// getting a list of translation approvals.
+type ApprovalsListResponse struct {
+	Data []*ApprovalsGetResponse `json:"data"`
+}
+
+// ApprovalsListOptions specifies the optional parameters to the
+// StringTranslationsService.ListApprovals method.
+type ApprovalsListOptions struct {
+	// File Identifier.
+	// Note: Must be used together with `languageId`.
+	FileID int `json:"fileId,omitempty"`
+	// Label Identifiers.
+	// Example: labelIds=1,2,3,4,5
+	LabelIDs []int `json:"labelIds,omitempty"`
+	// Exclude Label Identifiers.
+	ExcludeLabelIDs []int `json:"excludeLabelIds,omitempty"`
+	// String Identifier.
+	// Note: Must be used together with `languageId`.
+	StringID int `json:"stringId,omitempty"`
+	// Language Identifier.
+	// Note: Must be used together with `stringId` or `fileId`.
+	LanguageID string `json:"languageId,omitempty"`
+	// Translation Identifier.
+	// Note: If specified, `fileId`, `stringId` and `languageId` are ignored.
+	TranslationID int `json:"translationId,omitempty"`
+
+	ListOptions
+}
+
+// Values returns the url.Values representation of the ApprovalsListOptions.
+// It implements the crowdin.ListOptionsProvider interface.
+func (o *ApprovalsListOptions) Values() url.Values {
+	v := o.ListOptions.Values()
+	if o.FileID > 0 {
+		v.Add("fileId", fmt.Sprintf("%d", o.FileID))
+	}
+	if len(o.LabelIDs) > 0 {
+		v.Add("labelIds", joinIntSlice(o.LabelIDs))
+	}
+	if len(o.ExcludeLabelIDs) > 0 {
+		v.Add("excludeLabelIds", joinIntSlice(o.ExcludeLabelIDs))
+	}
+	if o.StringID > 0 {
+		v.Add("stringId", fmt.Sprintf("%d", o.StringID))
+	}
+	if o.LanguageID != "" {
+		v.Add("languageId", o.LanguageID)
+	}
+	if o.TranslationID > 0 {
+		v.Add("translationId", fmt.Sprintf("%d", o.TranslationID))
+	}
+
+	return v
+}
+
+// TranslationAlignment represents a translation alignment.
+type TranslationAlignment struct {
+	Words []*WordAlignment `json:"words"`
+}
+
+// WordAlignment represents a word alignments.
+type WordAlignment struct {
+	Text       string       `json:"text"`
+	Alignments []*Alignment `json:"alignments"`
+}
+
+// Alignment represents a word alignment.
+type Alignment struct {
+	SourceWord  string `json:"sourceWord"`
+	SourceLemma string `json:"sourceLemma"`
+	TargetWord  string `json:"targetWord"`
+	TargetLemma string `json:"targetLemma"`
+	Match       int    `json:"match"`
+	Probability int    `json:"probability"`
+}
+
+// TranslationAlignmentResponse defines the structure of the response when
+// aligning translations.
+type TranslationAlignmentResponse struct {
+	Data *TranslationAlignment `json:"data"`
+}
+
+// TranslationAlignmentRequest defines the structure of the request
+// to align translations.
+type TranslationAlignmentRequest struct {
+	// Source Language Identifier.
+	SourceLanguageID string `json:"sourceLanguageId"`
+	// Target Language Identifier.
+	TargetLanguageID string `json:"targetLanguageId"`
+	// Text for alignment.
+	Text string `json:"text"`
+}
+
+// Validate checks if the TranslationAlignmentRequest is valid.
+// It implements the crowdin.RequestValidator interface.
+func (r *TranslationAlignmentRequest) Validate() error {
+	if r == nil {
+		return fmt.Errorf("request cannot be nil")
+	}
+	if r.SourceLanguageID == "" {
+		return fmt.Errorf("source language ID is required")
+	}
+	if r.TargetLanguageID == "" {
+		return fmt.Errorf("target language ID is required")
+	}
+	if r.Text == "" {
+		return fmt.Errorf("text is required")
+	}
+	return nil
+}
+
+// LanguageTranslation represents a language translation.
+// Contains the plain, plural, or ICU translation.
+type LanguageTranslation struct {
+	StringID      int     `json:"stringId"`
+	ContentType   string  `json:"contentType"`
+	TranslationID *int    `json:"translationId,omitempty"`
+	Text          *string `json:"text,omitempty"`
+	User          *User   `json:"user,omitempty"`
+	CreatedAt     *string `json:"createdAt,omitempty"`
+
+	Plurals []*LanguageTranslationPlural `json:"plurals,omitempty"`
+}
+
+// LanguageTranslationPlural represents a plural language translation
+// and is part of the LanguageTranslation.
+type LanguageTranslationPlural struct {
+	TranslationID int    `json:"translationId"`
+	Text          string `json:"text"`
+	PluralForm    string `json:"pluralForm"`
+	User          User   `json:"user"`
+	CreatedAt     string `json:"createdAt"`
+}
+
+// LanguageTranslationsGetResponse defines the structure of the response when
+// retrieving a list of language translations.
+type LanguageTranslationsListResponse struct {
+	Data []struct {
+		Data *LanguageTranslation `json:"data"`
+	} `json:"data"`
+}
+
+// LanguageTranslationsListOptions specifies the optional parameters to the
+// StringTranslationsService.ListLanguageTranslations method.
+type LanguageTranslationsListOptions struct {
+	// String Identifiers. Filter translations by `stringIds`.
+	// Example: stringIds=1,2,3,4,5
+	StringIDs []int `json:"stringIds,omitempty"`
+	// Label Identifiers. Filter translations by `labelIds`.
+	// Example: labelIds=1,2,3,4,5
+	LabelIDs []int `json:"labelIds,omitempty"`
+	// File Identifier. Filter translations by `fileId`.
+	// Note: Can't be used with `branchId` or `directoryId` in the same request.
+	FileID int `json:"fileId,omitempty"`
+	// Branch Identifier. Filter translations by `branchId`.
+	// Note: Can't be used with `fileId` or `directoryId` in the same request.
+	BranchID int `json:"branchId,omitempty"`
+	// Directory Identifier. Filter translations by `directoryId`.
+	// Note: Can't be used with `fileId` or `branchId` in the same request.
+	DirectoryID int `json:"directoryId,omitempty"`
+	// Filter translations by CroQL.
+	// Note: Can't be used with `stringIds`, `labelIds` or `fileId`
+	// in the same request.
+	CroQL string `json:"croql,omitempty"`
+	// Enable denormalize placeholders.
+	// Enum: 0, 1. Default: 0.
+	DenormalizePlaceholders *int `json:"denormalizePlaceholders,omitempty"`
+
+	ListOptions
+}
+
+// Values returns the url.Values representation of the LanguageTranslationsListOptions.
+// It implements the crowdin.ListOptionsProvider interface.
+func (o *LanguageTranslationsListOptions) Values() url.Values {
+	v := o.ListOptions.Values()
+	if len(o.StringIDs) > 0 {
+		v.Add("stringIds", joinIntSlice(o.StringIDs))
+	}
+	if len(o.LabelIDs) > 0 {
+		v.Add("labelIds", joinIntSlice(o.LabelIDs))
+	}
+	if o.FileID > 0 {
+		v.Add("fileId", fmt.Sprintf("%d", o.FileID))
+	}
+	if o.BranchID > 0 {
+		v.Add("branchId", fmt.Sprintf("%d", o.BranchID))
+	}
+	if o.DirectoryID > 0 {
+		v.Add("directoryId", fmt.Sprintf("%d", o.DirectoryID))
+	}
+	if o.CroQL != "" {
+		v.Add("croql", o.CroQL)
+	}
+	if o.DenormalizePlaceholders != nil &&
+		(*o.DenormalizePlaceholders == 0 || *o.DenormalizePlaceholders == 1) {
+		v.Add("denormalizePlaceholders", fmt.Sprintf("%d", *o.DenormalizePlaceholders))
+	}
+
+	return v
+}
+
+// Translation represents a Crowdin translation.
+type Translation struct {
+	ID                 int     `json:"id"`
+	Text               string  `json:"text"`
+	PluralCategoryName string  `json:"pluralCategoryName"`
+	User               User    `json:"user"`
+	Rating             int     `json:"rating"`
+	Provider           *string `json:"provider,omitempty"`
+	IsPreTranslated    bool    `json:"isPreTranslated"`
+	CreatedAt          string  `json:"createdAt"`
+}
+
+// TranslationGetResponse defines the structure of the response when
+// getting a single translation.
+type TranslationGetResponse struct {
+	Data *Translation `json:"data"`
+}
+
+// TranslationsListResponse defines the structure of the response when
+// getting a list of translations.
+type TranslationsListResponse struct {
+	Data []*TranslationGetResponse `json:"data"`
+}
+
+// TranslationGetOptions specifies the optional parameters to the
+// StringTranslationsService.GetTranslation method.
+type TranslationGetOptions struct {
+	// Enable denormalize placeholders.
+	// Enum: 0, 1. Default: 0.
+	DenormalizePlaceholders *int `json:"denormalizePlaceholders,omitempty"`
+}
+
+// Values returns the url.Values representation of the TranslationGetOptions.
+// It implements the crowdin.ListOptionsProvider interface.
+func (o *TranslationGetOptions) Values() url.Values {
+	v := url.Values{}
+	if o.DenormalizePlaceholders != nil &&
+		(*o.DenormalizePlaceholders == 0 || *o.DenormalizePlaceholders == 1) {
+		v.Add("denormalizePlaceholders", fmt.Sprintf("%d", *o.DenormalizePlaceholders))
+	}
+	return v
+}
+
+// StringTranslationsListOptions specifies the optional parameters to the
+// StringTranslationsService.ListTranslations method.
+type StringTranslationsListOptions struct {
+	// String Identifier.
+	// Note: Must be used together with `languageId`.
+	StringID int `json:"stringId,omitempty"`
+	// Language Identifier.
+	// Note: Must be used together with `stringId`.
+	LanguageID string `json:"languageId,omitempty"`
+	// Denormalize Placeholders.
+	// Enum: 0, 1. Default: 0.
+	DenormalizePlaceholders *int `json:"denormalizePlaceholders,omitempty"`
+
+	ListOptions
+}
+
+// Values returns the url.Values representation of the StringTranslationsListOptions.
+// It implements the crowdin.ListOptionsProvider interface.
+func (o *StringTranslationsListOptions) Values() url.Values {
+	v := o.ListOptions.Values()
+	if o.StringID > 0 {
+		v.Add("stringId", fmt.Sprintf("%d", o.StringID))
+	}
+	if o.LanguageID != "" {
+		v.Add("languageId", o.LanguageID)
+	}
+	if o.DenormalizePlaceholders != nil &&
+		(*o.DenormalizePlaceholders == 0 || *o.DenormalizePlaceholders == 1) {
+		v.Add("denormalizePlaceholders", fmt.Sprintf("%d", *o.DenormalizePlaceholders))
+	}
+
+	return v
+}
+
+// TranslationAddRequest defines the structure of the request
+// to add a translation.
+type TranslationAddRequest struct {
+	// String Identifier.
+	// Note: Must be used together with `languageId`.
+	StringID int `json:"stringId"`
+	// Language Identifier.
+	// Note: Must be used together with `stringId`.
+	LanguageID string `json:"languageId"`
+	// Translation text.
+	Text string `json:"text"`
+	// Plural form. Enum: zero, one, two, few, many, and other.
+	// Note: Will be saved only if the source string has plurals and `pluralCategoryName`
+	// is equal to the one available for the language you add translations to.
+	PluralCategoryName string `json:"pluralCategoryName,omitempty"`
+}
+
+// Validate checks if the TranslationAddRequest is valid.
+// It implements the crowdin.RequestValidator interface.
+func (r *TranslationAddRequest) Validate() error {
+	if r == nil {
+		return fmt.Errorf("request cannot be nil")
+	}
+	if r.StringID == 0 {
+		return fmt.Errorf("string ID is required")
+	}
+	if r.LanguageID == "" {
+		return fmt.Errorf("language ID is required")
+	}
+	if r.Text == "" {
+		return fmt.Errorf("text is required")
+	}
+	return nil
+}
+
+// Vote represents a Crowdin translation vote.
+type Vote struct {
+	ID            int    `json:"id"`
+	User          User   `json:"user"`
+	TranslationID int    `json:"translationId"`
+	VotedAt       string `json:"votedAt"`
+	Mark          string `json:"mark"`
+}
+
+// VoteGetResponse defines the structure of the response when
+// getting a single translation vote.
+type VoteGetResponse struct {
+	Data *Vote `json:"data"`
+}
+
+// VotesListResponse defines the structure of the response when
+// getting a list of translation votes.
+type VotesListResponse struct {
+	Data []*VoteGetResponse `json:"data"`
+}
+
+// VotesListOptions specifies the optional parameters to the
+// StringTranslationsService.ListVotes method.
+type VotesListOptions struct {
+	// String Identifier.
+	// Note: Must be used together with `languageId`.
+	StringID int `json:"stringId,omitempty"`
+	// Language Identifier.
+	// Note: Must be used together with `stringId`.
+	LanguageID string `json:"languageId,omitempty"`
+	// Translation Identifier.
+	// Note: If specified, `stringId` and `languageId` are ignored.
+	TranslationID int `json:"translationId,omitempty"`
+	// File Identifier.
+	// Note: Must be used together with `languageId`.
+	FileID int `json:"fileId,omitempty"`
+	// Label Identifiers.
+	// Example: labelIds=1,2,3,4,5
+	LabelIDs []int `json:"labelIds,omitempty"`
+	// Exclude Label Identifiers.
+	EcludeLabelIDs []int `json:"excludeLabelIds,omitempty"`
+
+	ListOptions
+}
+
+// Values returns the url.Values representation of the VotesListOptions.
+// It implements the crowdin.ListOptionsProvider interface.
+func (o *VotesListOptions) Values() url.Values {
+	v := o.ListOptions.Values()
+	if o.StringID > 0 {
+		v.Add("stringId", fmt.Sprintf("%d", o.StringID))
+	}
+	if o.LanguageID != "" {
+		v.Add("languageId", o.LanguageID)
+	}
+	if o.TranslationID > 0 {
+		v.Add("translationId", fmt.Sprintf("%d", o.TranslationID))
+	}
+	if o.FileID > 0 {
+		v.Add("fileId", fmt.Sprintf("%d", o.FileID))
+	}
+	if len(o.LabelIDs) > 0 {
+		v.Add("labelIds", joinIntSlice(o.LabelIDs))
+	}
+	if len(o.EcludeLabelIDs) > 0 {
+		v.Add("excludeLabelIds", joinIntSlice(o.EcludeLabelIDs))
+	}
+
+	return v
+}
+
+// VoteType represents a translation vote type.
+type VoteType string
+
+const (
+	// VoteTypeUp is an upvote translation.
+	VoteTypeUp VoteType = "up"
+	// VoteTypeDown is a downvote translation.
+	VoteTypeDown VoteType = "down"
+)
+
+// VoteAddRequest defines the structure of the request
+// to add a translation vote.
+type VoteAddRequest struct {
+	// Enum: up, down.
+	Mark VoteType `json:"mark"`
+	// Translation Identifier.
+	TranslationID int `json:"translationId"`
+}
+
+// Validate checks if the VotesAddRequest is valid.
+// It implements the crowdin.RequestValidator interface.
+func (r *VoteAddRequest) Validate() error {
+	if r == nil {
+		return fmt.Errorf("request cannot be nil")
+	}
+	if r.Mark != VoteTypeUp && r.Mark != VoteTypeDown {
+		return fmt.Errorf("invalid vote type: %s", r.Mark)
+	}
+	if r.TranslationID == 0 {
+		return fmt.Errorf("translation ID is required")
+	}
+	return nil
+}
+
+func joinIntSlice(s []int) string {
+	var res []string
+	for _, v := range s {
+		res = append(res, fmt.Sprintf("%d", v))
+	}
+	return strings.Join(res, ",")
+}
---
+++ crowdin/string_translations.go
@@ -0,0 +1,235 @@
+package crowdin
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/crowdin/crowdin-api-client-go/crowdin/model"
+)
+
+// Use API to add or remove strings translations, approvals, and votes.
+//
+// Crowdin API docs: https://developer.crowdin.com/api/v2/#tag/String-Translations
+type StringTranslationsService struct {
+	client *Client
+}
+
+// ListApprovals returns a list of translation approvals.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.approvals.getMany
+func (s *StringTranslationsService) ListApprovals(ctx context.Context, projectId int, opts *model.ApprovalsListOptions) (
+	[]*model.Approval, *Response, error,
+) {
+	res := new(model.ApprovalsListResponse)
+	resp, err := s.client.Get(ctx, fmt.Sprintf("projects/%d/approvals", projectId), opts, res)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	list := make([]*model.Approval, 0, len(res.Data))
+	for _, approval := range res.Data {
+		list = append(list, approval.Data)
+	}
+
+	return list, resp, nil
+}
+
+// GetApproval returns a single translation approval by its identifier.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.approvals.get
+func (s *StringTranslationsService) GetApproval(ctx context.Context, projectID, approvalID int) (*model.Approval, *Response, error) {
+	res := new(model.ApprovalsGetResponse)
+	resp, err := s.client.Get(ctx, fmt.Sprintf("projects/%d/approvals/%d", projectID, approvalID), nil, res)
+
+	return res.Data, resp, err
+}
+
+// AddApproval adds a new translation approval.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.approvals.post
+func (s *StringTranslationsService) AddApproval(ctx context.Context, projectID, translationID int) (
+	*model.Approval, *Response, error,
+) {
+	req := struct {
+		TranslationID int `json:"translationId"`
+	}{TranslationID: translationID}
+
+	res := new(model.ApprovalsGetResponse)
+	resp, err := s.client.Post(ctx, fmt.Sprintf("projects/%d/approvals", projectID), req, res)
+
+	return res.Data, resp, err
+}
+
+// RemoveApproval removes a translation approval by its identifier.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.approvals.delete
+func (s *StringTranslationsService) RemoveApproval(ctx context.Context, projectID, approvalID int) (*Response, error) {
+	return s.client.Delete(ctx, fmt.Sprintf("projects/%d/approvals/%d", projectID, approvalID))
+}
+
+// TranslationAlignment aligns translations.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.translations.alignment.post
+func (s *StringTranslationsService) TranslationAlignment(ctx context.Context, projectID int, req *model.TranslationAlignmentRequest) (
+	*model.TranslationAlignment, *Response, error,
+) {
+	res := new(model.TranslationAlignmentResponse)
+	resp, err := s.client.Post(ctx, fmt.Sprintf("projects/%d/translations/alignment", projectID), req, res)
+
+	return res.Data, resp, err
+}
+
+// ListLanguageTranslations returns a list of translations for a specific language.
+//
+// Note: For instant translation delivery to your mobile, web, server, or desktop apps,
+// it is recommended to use OTA.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.languages.translations.getMany
+func (s *StringTranslationsService) ListLanguageTranslations(
+	ctx context.Context,
+	projectID int,
+	languageID string,
+	opts *model.LanguageTranslationsListOptions) (
+	[]*model.LanguageTranslation, *Response, error,
+) {
+	res := new(model.LanguageTranslationsListResponse)
+	resp, err := s.client.Get(ctx, fmt.Sprintf("projects/%d/languages/%s/translations", projectID, languageID), opts, res)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	list := make([]*model.LanguageTranslation, 0, len(res.Data))
+	for _, translation := range res.Data {
+		list = append(list, translation.Data)
+	}
+
+	return list, resp, nil
+}
+
+// ListStringTranslations returns a list of string translations.
+//
+// Note: For instant translation delivery to your mobile, web, server, or desktop apps,
+// it is recommended to use OTA.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.translations.getMany
+func (s *StringTranslationsService) ListStringTranslations(ctx context.Context, projectID int, opts *model.StringTranslationsListOptions) (
+	[]*model.Translation, *Response, error,
+) {
+	res := new(model.TranslationsListResponse)
+	resp, err := s.client.Get(ctx, fmt.Sprintf("projects/%d/translations", projectID), opts, res)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	list := make([]*model.Translation, 0, len(res.Data))
+	for _, translation := range res.Data {
+		list = append(list, translation.Data)
+	}
+
+	return list, resp, nil
+}
+
+// DeleteStringTranslations deletes string translations by its identifiers.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.translations.deleteMany
+func (s *StringTranslationsService) DeleteStringTranslations(ctx context.Context, projectID, stringID int, languageID string) (
+	*Response, error,
+) {
+	path := fmt.Sprintf("projects/%d/translations?stringId=%d&languageId=%s", projectID, stringID, languageID)
+	return s.client.Delete(ctx, path)
+}
+
+// GetTranslation returns a single string translation by its identifier.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.translations.get
+func (s *StringTranslationsService) GetTranslation(ctx context.Context, projectID, translationID int, opts *model.TranslationGetOptions) (
+	*model.Translation, *Response, error,
+) {
+	res := new(model.TranslationGetResponse)
+	resp, err := s.client.Get(ctx, fmt.Sprintf("projects/%d/translations/%d", projectID, translationID), opts, res)
+
+	return res.Data, resp, err
+}
+
+// AddTranslation adds a new string translation.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.translations.post
+func (s *StringTranslationsService) AddTranslation(ctx context.Context, projectID int, req *model.TranslationAddRequest) (
+	*model.Translation, *Response, error,
+) {
+	res := new(model.TranslationGetResponse)
+	resp, err := s.client.Post(ctx, fmt.Sprintf("projects/%d/translations", projectID), req, res)
+
+	return res.Data, resp, err
+}
+
+// RestoreTranslation restores a translation by its identifier.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.translations.put
+func (s *StringTranslationsService) RestoreTranslation(ctx context.Context, projectID, translationID int) (
+	*model.Translation, *Response, error,
+) {
+	res := new(model.TranslationGetResponse)
+	resp, err := s.client.Put(ctx, fmt.Sprintf("projects/%d/translations/%d", projectID, translationID), nil, res)
+
+	return res.Data, resp, err
+}
+
+// DeleteTranslation deletes a translation by its identifier.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.translations.delete
+func (s *StringTranslationsService) DeleteTranslation(ctx context.Context, projectID, translationID int) (*Response, error) {
+	return s.client.Delete(ctx, fmt.Sprintf("projects/%d/translations/%d", projectID, translationID))
+}
+
+// ListVotes lists translation votes.
+//
+// Note: Either `translationId` OR `fileId` OR `labelIds` OR `excludeLabelIds` with
+// `languageId` OR `stringId` with `languageId` are required
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.votes.getMany
+func (s *StringTranslationsService) ListVotes(ctx context.Context, projectID int, opts *model.VotesListOptions) (
+	[]*model.Vote, *Response, error,
+) {
+	res := new(model.VotesListResponse)
+	resp, err := s.client.Get(ctx, fmt.Sprintf("projects/%d/votes", projectID), opts, res)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	list := make([]*model.Vote, 0, len(res.Data))
+	for _, vote := range res.Data {
+		list = append(list, vote.Data)
+	}
+
+	return list, resp, nil
+}
+
+// GetVote gets a single translation vote by its identifier.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.votes.get
+func (s *StringTranslationsService) GetVote(ctx context.Context, projectID, voteID int) (*model.Vote, *Response, error) {
+	res := new(model.VoteGetResponse)
+	resp, err := s.client.Get(ctx, fmt.Sprintf("projects/%d/votes/%d", projectID, voteID), nil, res)
+
+	return res.Data, resp, err
+}
+
+// AddVote adds a vote for a translation.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.votes.post
+func (s *StringTranslationsService) AddVote(ctx context.Context, projectID int, req *model.VoteAddRequest) (
+	*model.Vote, *Response, error,
+) {
+	res := new(model.VoteGetResponse)
+	resp, err := s.client.Post(ctx, fmt.Sprintf("projects/%d/votes", projectID), req, res)
+
+	return res.Data, resp, err
+}
+
+// CancelVote cancels a vote for a translation by its identifier.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.votes.delete
+func (s *StringTranslationsService) CancelVote(ctx context.Context, projectID, voteID int) (*Response, error) {
+	return s.client.Delete(ctx, fmt.Sprintf("projects/%d/votes/%d", projectID, voteID))
+}
