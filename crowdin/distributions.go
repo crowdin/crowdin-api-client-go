843499ddc4a536ba48a32f193aac09cd4649a838
ches <1688276+vorobeyme@users.noreply.github.com>
2024-05-24 10:41:16 +0300

    feat: add Distributions API methods (#41)

---
+++ crowdin/distributions.go
@@ -0,0 +1,105 @@
+package crowdin
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/crowdin/crowdin-api-client-go/crowdin/model"
+)
+
+// DistributionsService provides access to the Distributions API methods.
+//
+// Crowdin API docs: https://developer.crowdin.com/api/v2/#tag/Distributions
+type DistributionsService struct {
+	client *Client
+}
+
+// List returns a list of distributions in the project.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.distributions.getMany
+func (s *DistributionsService) List(ctx context.Context, projectID int, opts *model.ListOptions) (
+	[]*model.Distribution, *Response, error,
+) {
+	res := new(model.DistributionsListResponse)
+	resp, err := s.client.Get(ctx, fmt.Sprintf("/api/v2/projects/%d/distributions", projectID), opts, res)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	list := make([]*model.Distribution, 0, len(res.Data))
+	for _, item := range res.Data {
+		list = append(list, item.Data)
+	}
+
+	return list, resp, nil
+}
+
+// Get returns information about a distribution.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.distributions.get
+func (s *DistributionsService) Get(ctx context.Context, projectID int, hash string) (*model.Distribution, *Response, error) {
+	res := new(model.DistributionResponse)
+	resp, err := s.client.Get(ctx, fmt.Sprintf("/api/v2/projects/%d/distributions/%s", projectID, hash), nil, res)
+
+	return res.Data, resp, err
+}
+
+// Add creates a new distribution.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.distributions.post
+func (s *DistributionsService) Add(ctx context.Context, projectID int, req *model.DistributionAddRequest) (
+	*model.Distribution, *Response, error,
+) {
+	res := new(model.DistributionResponse)
+	resp, err := s.client.Post(ctx, fmt.Sprintf("/api/v2/projects/%d/distributions", projectID), req, res)
+
+	return res.Data, resp, err
+}
+
+// Edit updates a distribution in the project.
+//
+// Request body:
+//   - Op (string) - Operation to perform. Enum: replace.
+//   - Path (string) - JSON path to the field to update. Enum: "/exportMode", "/name", "/fileIds", "/bundleIds".
+//   - Value (string) - New alue to set.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.distributions.patch
+func (s *DistributionsService) Edit(ctx context.Context, projectID int, hash string, req []*model.UpdateRequest) (
+	*model.Distribution, *Response, error,
+) {
+	res := new(model.DistributionResponse)
+	resp, err := s.client.Patch(ctx, fmt.Sprintf("/api/v2/projects/%d/distributions/%s", projectID, hash), req, res)
+
+	return res.Data, resp, err
+}
+
+// Delete removes a distribution from the project.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.distributions.delete
+func (s *DistributionsService) Delete(ctx context.Context, projectID int, hash string) (*Response, error) {
+	return s.client.Delete(ctx, fmt.Sprintf("/api/v2/projects/%d/distributions/%s", projectID, hash))
+}
+
+// GetRelease returns information about the distribution release.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.distributions.release.get
+func (s *DistributionsService) GetRelease(ctx context.Context, projectID int, hash string) (
+	*model.DistributionRelease, *Response, error,
+) {
+	res := new(model.DistributionReleaseResponse)
+	resp, err := s.client.Get(ctx, fmt.Sprintf("/api/v2/projects/%d/distributions/%s/release", projectID, hash), nil, res)
+
+	return res.Data, resp, err
+}
+
+// Release releases the distribution.
+//
+// https://developer.crowdin.com/api/v2/#operation/api.projects.distributions.release.post
+func (s *DistributionsService) Release(ctx context.Context, projectID int, hash string) (
+	*model.DistributionRelease, *Response, error,
+) {
+	res := new(model.DistributionReleaseResponse)
+	resp, err := s.client.Post(ctx, fmt.Sprintf("/api/v2/projects/%d/distributions/%s/release", projectID, hash), "", res)
+
+	return res.Data, resp, err
+}
---
+++ crowdin/distributions_test.go
@@ -0,0 +1,385 @@
+package crowdin
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/crowdin/crowdin-api-client-go/crowdin/model"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDistributionsService_List(t *testing.T) {
+	tests := []struct {
+		name          string
+		opts          *model.ListOptions
+		expectedQuery string
+	}{
+		{
+			name:          "nil options",
+			opts:          nil,
+			expectedQuery: "",
+		},
+		{
+			name:          "empty options",
+			opts:          &model.ListOptions{},
+			expectedQuery: "",
+		},
+		{
+			name:          "all options",
+			opts:          &model.ListOptions{Limit: 10, Offset: 5},
+			expectedQuery: "?limit=10&offset=5",
+		},
+	}
+
+	client, mux, teardown := setupClient()
+	defer teardown()
+
+	for projectID, tt := range tests {
+		path := fmt.Sprintf("/api/v2/projects/%d/distributions", projectID)
+		mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, http.MethodGet)
+			testURL(t, r, path+tt.expectedQuery)
+
+			fmt.Fprint(w, `{
+				"data": [
+					{
+						"data": {
+							"hash": "50fb350641274ba88296f97dc7e3e0c3",
+							"name": "Export Bundle",
+							"bundleIds": [45, 62],
+							"createdAt": "2023-09-16T13:48:04+00:00",
+							"updatedAt": "2023-09-19T13:25:27+00:00",
+							"exportMode": "bundle",
+							"fileIds": [24, 25, 38]
+						}						
+					},
+					{
+						"data": {
+							"hash": "50fb350641274ba88296f97dc7e3e0c4",
+							"name": "Export Bundle",
+							"bundleIds": [47],
+							"createdAt": "2023-09-16T13:48:04+00:00",
+							"updatedAt": "2023-09-19T13:25:27+00:00",
+							"exportMode": "bundle",
+							"fileIds": [25]
+						}
+					}
+				],
+				"pagination": {
+					"offset": 1,
+					"limit": 2
+				}
+			}`)
+		})
+
+		distributions, resp, err := client.Distributions.List(context.Background(), projectID, tt.opts)
+		require.NoError(t, err)
+		assert.NotNil(t, resp)
+
+		expected := []*model.Distribution{
+			{
+				Hash:       "50fb350641274ba88296f97dc7e3e0c3",
+				Name:       "Export Bundle",
+				BundleIDs:  []int{45, 62},
+				CreatedAt:  "2023-09-16T13:48:04+00:00",
+				UpdatedAt:  "2023-09-19T13:25:27+00:00",
+				ExportMode: "bundle",
+				FileIDs:    []int{24, 25, 38},
+			},
+			{
+				Hash:       "50fb350641274ba88296f97dc7e3e0c4",
+				Name:       "Export Bundle",
+				BundleIDs:  []int{47},
+				CreatedAt:  "2023-09-16T13:48:04+00:00",
+				UpdatedAt:  "2023-09-19T13:25:27+00:00",
+				ExportMode: "bundle",
+				FileIDs:    []int{25},
+			},
+		}
+		require.Equal(t, expected, distributions)
+	}
+}
+
+func TestDistributionsService_Get(t *testing.T) {
+	client, mux, teardown := setupClient()
+	defer teardown()
+
+	const path = "/api/v2/projects/1/distributions/50fb350641274ba88296f97dc7e3e0c3"
+	mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		testURL(t, r, path)
+
+		fmt.Fprint(w, `{
+			"data": {
+				"hash": "50fb350641274ba88296f97dc7e3e0c3",
+				"name": "Export Bundle",
+				"bundleIds": [45, 62],
+				"createdAt": "2023-09-16T13:48:04+00:00",
+				"updatedAt": "2023-09-19T13:25:27+00:00",
+				"exportMode": "bundle",
+				"fileIds": [24, 25, 38]
+			}
+		}`)
+	})
+
+	distribution, resp, err := client.Distributions.Get(context.Background(), 1, "50fb350641274ba88296f97dc7e3e0c3")
+	require.NoError(t, err)
+	assert.NotNil(t, resp)
+
+	expected := &model.Distribution{
+		Hash:       "50fb350641274ba88296f97dc7e3e0c3",
+		Name:       "Export Bundle",
+		BundleIDs:  []int{45, 62},
+		CreatedAt:  "2023-09-16T13:48:04+00:00",
+		UpdatedAt:  "2023-09-19T13:25:27+00:00",
+		ExportMode: "bundle",
+		FileIDs:    []int{24, 25, 38},
+	}
+	require.Equal(t, expected, distribution)
+}
+
+func TestDistributionsService_Add(t *testing.T) {
+	client, mux, teardown := setupClient()
+	defer teardown()
+
+	const path = "/api/v2/projects/1/distributions"
+	mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+		testURL(t, r, path)
+		testBody(t, r, `{"name":"Export Bundle","exportMode":"bundle","bundleIds":[45,62]}`+"\n")
+
+		w.WriteHeader(http.StatusCreated)
+		fmt.Fprint(w, `{
+			"data": {
+				"hash": "50fb350641274ba88296f97dc7e3e0c3",
+				"name": "Export Bundle",
+				"bundleIds": [45, 62],
+				"createdAt": "2023-09-16T13:48:04+00:00",
+				"updatedAt": "2023-09-19T13:25:27+00:00",
+				"exportMode": "bundle",
+				"fileIds": [24, 25, 38]
+			}
+		}`)
+	})
+
+	req := &model.DistributionAddRequest{
+		Name:       "Export Bundle",
+		ExportMode: model.ExportModeBundle,
+		BundleIDs:  []int{45, 62},
+	}
+	distribution, resp, err := client.Distributions.Add(context.Background(), 1, req)
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+
+	expected := &model.Distribution{
+		Hash:       "50fb350641274ba88296f97dc7e3e0c3",
+		Name:       "Export Bundle",
+		BundleIDs:  []int{45, 62},
+		CreatedAt:  "2023-09-16T13:48:04+00:00",
+		UpdatedAt:  "2023-09-19T13:25:27+00:00",
+		ExportMode: "bundle",
+		FileIDs:    []int{24, 25, 38},
+	}
+	assert.Equal(t, expected, distribution)
+}
+
+func TestDistributionsService_Add_requestValidation(t *testing.T) {
+	tests := []struct {
+		name string
+		req  *model.DistributionAddRequest
+		err  string
+	}{
+		{
+			name: "nil request",
+			req:  nil,
+			err:  "request cannot be nil",
+		},
+		{
+			name: "empty name",
+			req:  &model.DistributionAddRequest{},
+			err:  "name is required",
+		},
+		{
+			name: "empty bundleIds",
+			req: &model.DistributionAddRequest{
+				Name:       "Export Bundle",
+				ExportMode: model.ExportModeBundle,
+				FileIDs:    []int{24, 25, 38},
+			},
+			err: "bundleIds is required for bundle export mode",
+		},
+		{
+			name: "empty fileIds",
+			req: &model.DistributionAddRequest{
+				Name:       "Export Bundle",
+				ExportMode: model.ExportModeDefault,
+			},
+			err: "fileIds is required for default export mode",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.EqualError(t, tt.req.Validate(), tt.err)
+		})
+	}
+}
+
+func TestDistributionsService_Edit(t *testing.T) {
+	client, mux, teardown := setupClient()
+	defer teardown()
+
+	const path = "/api/v2/projects/1/distributions/50fb350641274ba88296f97dc7e3e0c3"
+	mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPatch)
+		testURL(t, r, path)
+		testBody(t, r, `[{"op":"replace","path":"/exportMode","value":"bundle"}]`+"\n")
+
+		fmt.Fprint(w, `{
+			"data": {
+				"hash": "50fb350641274ba88296f97dc7e3e0c3",
+				"name": "Export Bundle",
+				"bundleIds": [45, 62],
+				"createdAt": "2023-09-16T13:48:04+00:00",
+				"updatedAt": "2023-09-19T13:25:27+00:00",
+				"exportMode": "bundle",
+				"fileIds": [24, 25, 38]
+			}
+		}`)
+	})
+
+	req := []*model.UpdateRequest{
+		{
+			Op:    "replace",
+			Path:  "/exportMode",
+			Value: "bundle",
+		},
+	}
+	distribution, resp, err := client.Distributions.Edit(context.Background(), 1, "50fb350641274ba88296f97dc7e3e0c3", req)
+	require.NoError(t, err)
+	assert.NotNil(t, resp)
+
+	expected := &model.Distribution{
+		Hash:       "50fb350641274ba88296f97dc7e3e0c3",
+		Name:       "Export Bundle",
+		BundleIDs:  []int{45, 62},
+		CreatedAt:  "2023-09-16T13:48:04+00:00",
+		UpdatedAt:  "2023-09-19T13:25:27+00:00",
+		ExportMode: "bundle",
+		FileIDs:    []int{24, 25, 38},
+	}
+	assert.Equal(t, expected, distribution)
+}
+
+func TestDistributionsService_Delete(t *testing.T) {
+	client, mux, teardown := setupClient()
+	defer teardown()
+
+	const path = "/api/v2/projects/1/distributions/50fb350641274ba88296f97dc7e3e0c3"
+	mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodDelete)
+		testURL(t, r, path)
+
+		w.WriteHeader(http.StatusNoContent)
+	})
+
+	resp, err := client.Distributions.Delete(context.Background(), 1, "50fb350641274ba88296f97dc7e3e0c3")
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusNoContent, resp.StatusCode)
+}
+
+func TestDistributionsService_GetRelease(t *testing.T) {
+	tests := []struct {
+		name     string
+		jsonResp string
+		expected *model.DistributionRelease
+	}{
+		{
+			name: "all fields",
+			jsonResp: `{
+				"data": {
+					"status": "success",
+					"progress": 100,
+					"currentLanguageId": "uk",
+					"date": "2023-09-23T09:04:29+00:00",
+					"currentFileId": 8
+				}
+			}`,
+			expected: &model.DistributionRelease{
+				Status:            "success",
+				Progress:          100,
+				CurrentLanguageID: "uk",
+				Date:              "2023-09-23T09:04:29+00:00",
+				CurrentFileID:     8,
+			},
+		},
+		{
+			name: "nullable fields",
+			jsonResp: `{
+				"data": {
+					"status": null,
+					"progress": null,
+					"currentLanguageId": null,
+					"date": null,
+					"currentFileId": null
+				}
+			}`,
+			expected: &model.DistributionRelease{},
+		},
+	}
+
+	client, mux, teardown := setupClient()
+	defer teardown()
+
+	for projectID, tt := range tests {
+		path := fmt.Sprintf("/api/v2/projects/%d/distributions/50fb350641274ba88296f97dc7e3e0c3/release", projectID)
+		mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, http.MethodGet)
+			testURL(t, r, path)
+
+			fmt.Fprint(w, tt.jsonResp)
+		})
+
+		release, resp, err := client.Distributions.GetRelease(context.Background(), projectID, "50fb350641274ba88296f97dc7e3e0c3")
+		require.NoError(t, err)
+		assert.NotNil(t, resp)
+		require.Equal(t, tt.expected, release)
+	}
+}
+
+func TestDistributionsService_Release(t *testing.T) {
+	client, mux, teardown := setupClient()
+	defer teardown()
+
+	const path = "/api/v2/projects/1/distributions/50fb350641274ba88296f97dc7e3e0c3/release"
+	mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+		testURL(t, r, path)
+
+		fmt.Fprint(w, `{
+			"data": {
+				"status": "success",
+				"progress": 100,
+				"currentLanguageId": "uk",
+				"date": "2023-09-23T09:04:29+00:00",
+				"currentFileId": 8
+			}
+		}`)
+	})
+
+	release, resp, err := client.Distributions.Release(context.Background(), 1, "50fb350641274ba88296f97dc7e3e0c3")
+	require.NoError(t, err)
+	assert.NotNil(t, resp)
+
+	expected := &model.DistributionRelease{
+		Status:            "success",
+		Progress:          100,
+		CurrentLanguageID: "uk",
+		Date:              "2023-09-23T09:04:29+00:00",
+		CurrentFileID:     8,
+	}
+	require.Equal(t, expected, release)
+}
