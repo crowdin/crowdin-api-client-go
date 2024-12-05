febfa537c918c298157202440c9411a097290b99
Andrii Bodnar <andrii.bodnar@crowdin.com>
2024-12-05 15:16:01 +0200

    chore: version 0.11.0

--- crowdin/crowdin.go
+++ crowdin/crowdin.go
@@ -17,7 +17,7 @@ import (
 const (
 	baseURL = "https://api.crowdin.com/"
 
-	userAgent = "crowdin-api-client-go/0.10.0"
+	userAgent = "crowdin-api-client-go/0.11.0"
 )
 
 // Client is a Crowdin API client.
